eedbba470a9b33f738655674a1ed026aa262c324
Guira <guira@Guiras-MacBook-Pro.local>
2020-09-19 19:19:09 +0200

    parte-infernal

--- bin/seed.js
+++ bin/seed.js
@@ -6,7 +6,7 @@ const Song = require("../models/song.model");
 const Picture = require("../models/picture.model");
 
 const dbtitle = "PROYECTO";
-mongoose.connect(`mongodb://localhost/${dbtitle}`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb://localhost/${dbtitle}`);
 
 Goal.collection.drop();
 Sentence.collection.drop();
@@ -16,86 +16,37 @@ Picture.collection.drop();
 const goals = [
   {
     name: "My garden",
-
-    theme: ["hobbies", "mental health", "home", "garden"],
-
-    sentence: [
-        {
-          "kind": "quote",
-
-          "theme": ["hobbies", "garden", "social"],
-
-          "phrase":
-            "A society grows great when old men plant trees whose shade they know they shall never sit in.",
-
-          "author": "Greek proverb",
-        },
-        {
-          "kind": "quote",
-
-          "theme": ["work", "garden", "social"],
-
-          "phrase":
-            "Working with plants will teach you all other social commitments in a soothing way",
-
-          "author": "Karthikeyan V",
-        },
-    ],
-
-        //picture: [ ""],
-
-      //song: [""],
-    
+    theme: ["hobbies", "health", "home"],
+    sentence: [{
+        kind: "quote",
+        theme: ["hobbies", "social"],
+        phrase:
+          "A society grows great when old men plant trees whose shade they know they shall never sit in.",
+        author: "Greek proverb"
+    },{
+        kind: "quote",
+        theme: ["hobbies", "social"],
+        phrase:
+          "I want to water my plants on sunday morning.",
+        author: "Laura Li"
+    }]
+  },{
+    name: "Working out",
+    theme: ["hobbies", "health"],
+    sentence: [{
+        kind: "quote",
+        theme: ["health"],
+        phrase:
+          "Don't let anyone work harden than you do",
+        author: ""
+    }]
   },
 ];
 
-const createSentences = goals.map((goal) => {
-  const newSentence = new Sentence(goal.sentence);
-  return newSentence
-      .save()
-      .then((sentence) => sentence.phrase)
-      .then((sentence)=>console.log(sentence))
-    .catch((err) => {
-      throw new Error(`Impossible to add the quote. ${err}`);
-    });
-});
 
-let findSentences = Promise.all(createSentences)
-  .then((sentence) => {
-    return goals.map((goal) => {
-      return Sentence.findOne({
-        kind: goal.sentence.kind,
-        theme: [goal.sentence.theme],
-        phrase: goal.sentence.phrase,
-        author: goal.sentence.author,
-      }).then((sentence) => {
-        if (!sentence) {
-          throw new Error(
-            `unknown phrase ${goal.sentence.phrase} by ${goal.sentence.author}`
-          );
-        }
-        return Object.assign({}, goal, { sentence: sentence._id });
-      });
-    });
-  })
-  .catch((error) => {
-    throw new Error(error);
-  });
+Promise.all(goals.map(goal => Sentence.create(goal.sentence).then(sentence => sentence.phrase)))
+    .then(() => goals.map(goal => Sentence.findOne({ phrase: goal.sentence.phrase }).then(goal => Object.assign({}, sentence, { goal: goal._id }))))
+    .then(findSentences => Promise.all(findSentences).then(goals => goals.map(goal => goal.create(goal))))
+    .then(savedGoals => Promise.all(savedGoals).then(goals => goals.forEach(goals => console.log(`Goal: ${goals.name} creado`))).then(() => mongoose.connection.close()))
+    .catch(error => console.log('Error: ', error))
 
-const saveGoals = findSentences
-  .then((findSentences) => {
-    return Promise.all(findSentences).then((goals) => {
-      return goals.map((goal) => {
-        const newGoal = new Goal(goal);
-        return newGoal.save();
-      });
-    });
-  })
-  .then((savedGoals) => {
-    Promise.all(savedGoals)
-      .then((goals) =>
-        goals.forEach((goal) => console.log(`created ${goal.name}`))
-      )
-      .then(() => mongoose.connection.close())
-      .catch((err) => console.log("Error while saving the goal: ", err));
-  });
