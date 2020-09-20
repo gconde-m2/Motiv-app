64c5cf9c164f724d0ed0e424b9eefd1ca1786027
Guira <guira@Guiras-MacBook-Pro.local>
2020-09-20 17:24:34 +0200

    registerOK

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,7 +18,7 @@ router.get("/registro", (req, res, next) => res.render("signup-form"))
 router.post("/registro",(req,res,next) => {
 
     const {username,password} = req.body
-
+  
     if (username.length === 0 || password.length === 0) {
       res.render("signup-form", { errorMessage: "Indicate username and password" })
       return
@@ -29,13 +29,16 @@ router.post("/registro",(req,res,next) => {
               res.render("signup-form", { errorMessage: "The username already exists" })
               return
           }
-  })
-  const salt = bcrypt.genSaltSync(bcryptSalt)
+          else{
+            const salt = bcrypt.genSaltSync(bcryptSalt)
   const hashPass = bcrypt.hashSync(password, salt)
 
   User.create({username,password:hashPass})
   .then(() => res.redirect('/'))
   .catch(error => next(error))
+          }
+  })
+  
 })
 //log in!!!!!
 
